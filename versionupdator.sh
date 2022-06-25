db81b7a0c36b5d4adfa0d1f0ac18520882c30734
Matthew Fitzgerald <31381527+mfitzgerald2@users.noreply.github.com>
2022-06-25 14:12:04 -0500

    Set unattended install further up.

--- versionupdator.sh
+++ versionupdator.sh
@@ -2,6 +2,8 @@
 echo -n "STARTING UPDATE SCRIPTS"
 apt-get -y install lsb-release
 version=`lsb_release -sr`
+source /opt/unattendedconfig.sh
+export APT_LISTCHANGES_FRONTEND=none
 
 case $version in 
     
@@ -9,9 +11,9 @@ case $version in
         # 8.8 to 8.11
         echo -n "Updating Debian 8 to 8.11"
         apt-get update
-        apt-get -y upgrade
+        apt-get -y -o Dpkg::Options::="--force-confnew" upgrade
         apt --purge autoremove
-        apt-get -y dist-upgrade
+        apt-get -y -o Dpkg::Options::="--force-confnew" dist-upgrade
         apt --purge autoremove
         reboot
         ;;
@@ -22,9 +24,9 @@ case $version in
         cp /etc/apt/sources.list /etc/apt/sources.list_backup
         sed -i 's/jessie/stretch/g' /etc/apt/sources.list
         apt-get update
-        apt-get -y upgrade
+        apt-get -y -o Dpkg::Options::="--force-confnew" upgrade
         apt --purge autoremove
-        apt-get -y dist-upgrade
+        apt-get -y -o Dpkg::Options::="--force-confnew" dist-upgrade
         apt --purge autoremove
         reboot
         ;;
@@ -33,13 +35,13 @@ case $version in
         # 9 to 10
         echo -n "Updating Debian 9 to 10"
         apt-get update
-        apt-get -y upgrade
+        apt-get -y -o Dpkg::Options::="--force-confnew" upgrade
         apt --purge autoremove
         cp /etc/apt/sources.list /etc/apt/sources.list_backup
         sed -i 's/stretch/buster/g' /etc/apt/sources.list
         apt-get update
-        apt-get -y upgrade
-        apt -y full-upgrade
+        apt-get -y -o Dpkg::Options::="--force-confnew" upgrade
+        apt -y -o Dpkg::Options::="--force-confnew" full-upgrade
         apt --purge autoremove
         reboot
         ;;
@@ -48,15 +50,15 @@ case $version in
         # 10 to 11
         echo -n "Updating Debian 10 to 11"
         apt-get update
-        apt-get -y upgrade
+        apt-get -y -o Dpkg::Options::="--force-confnew" upgrade
         apt --purge autoremove
         cp /etc/apt/sources.list /etc/apt/sources.list_backup
         sed -i 's/buster/bullseye/g' /etc/apt/sources.list
         sed -i 's/buster/bullseye/g' /etc/apt/sources.list.d/*.list
         sed -i 's#/debian-security bullseye/updates# bullseye-security#g' /etc/apt/sources.list
         apt-get update
-        apt-get -y upgrade
-        apt -y full-upgrade
+        apt-get -y -o Dpkg::Options::="--force-confnew" upgrade
+        apt -y -o Dpkg::Options::="--force-confnew" full-upgrade
         apt --purge autoremove
         reboot
         ;;
@@ -64,6 +66,5 @@ case $version in
     11)
         #Remove script
         rm -y /etc/systemd/system/versionupdator.service
-        ./opt/debiantolatest/rebunattendedconfig.sh
         ;;
 esac
